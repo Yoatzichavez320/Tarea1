8ddef57604406aba884697f3ea913cb261644da7
BERENICE CHAVEZ <159495624+Yoatzichavez320@users.noreply.github.com>
2024-03-10 19:37:43 -0600

    Delete if2python.py

--- if2python.py
+++
@@ -1,4 +0,0 @@
-# Solicitar al usuario un número de cliente. Si el número es el 1000, imprimir "Ganaste un premio"
-numero_cliente = 1000
-if numero_cliente == 1000: 
-    print("ganaste un premio") 
